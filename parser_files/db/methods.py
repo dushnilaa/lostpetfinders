f11679de82372cfd103afde75630967c237c231f
dushnilaa <uncecess.nick@gmail.com>
2023-01-07 14:20:01 +0500

    removed updating existing records

--- parser_files/db/methods.py
+++ parser_files/db/methods.py
@@ -28,14 +28,15 @@ class MethodsMySQL:
             self.session.commit()
             self.session.close()
         except IntegrityError:
-            self.update(dict_insert, class_table)
+            self.session.close()
+            # self.update(dict_insert, class_table)
 
-    def update(self, dict_insert, class_table):
-        self.session.rollback()
-        self.session.execute(
-            update(class_table)
-            .where(class_table.ws_id == dict_insert.get('ws_id'))
-            .values(**dict_insert)
-        )
-        self.session.commit()
-        self.session.close()
+    # def update(self, dict_insert, class_table):
+    #     self.session.rollback()
+    #     self.session.execute(
+    #         update(class_table)
+    #         .where(class_table.ws_id == dict_insert.get('ws_id'))
+    #         .values(**dict_insert)
+    #     )
+    #     self.session.commit()
+    #     self.session.close()
